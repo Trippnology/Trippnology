72e4c5be83f012435bc61c8a1063f3da926cfea5
Rikki Tripp <mail@trippnology.com>
2012-10-08 17:55:10 +0100

    Copied strings file and modified syntax

---
+++ inc/strings.php
@@ -0,0 +1,47 @@
+<?php
+// Global
+//Dim strDomain, strAdminEmail, strSiteName, strTagline, strBootswatch, strResponsive, strUserIP
+$strDomain = "http://localhost/bootstrasp/";
+$strAdminEmail = "";
+$strSiteName = "BootstrASP";
+$strTagline = "Twitter Bootstrap with some ASP goodies";
+$strBootswatch = ""; // Should we load a Bootswatch?;
+$strResponsive = true; // Load responsive stylesheet?;
+$strUserIP = Request.ServerVariables("REMOTE_ADDR");
+
+// Page Specific
+// You could set default values here and overide them per page
+//Dim strPageName, strCanonical, strDescription, strKeywords
+
+//Bootstrap
+//Dim strBSCSS, strBSImg, strBSJS
+$strBSCSS = "bs/docs/assets/css/";
+$strBSImg = "bs/docs/assets/img/";
+$strBSJS = "bs/docs/assets/js/";
+
+// External sites
+//Dim strTwitterName, strTweetText, strFacebookURL, strGoogleAnalID, strGoogleFonts
+$strTwitterName = "Trippnology";
+$strFacebookURL = "http://www.facebook.com/Trippnology";
+$strGoogleAnalID = "UA-XXXXX-X";
+$strGoogleFonts = "Squada+One";
+
+// Form to email
+// Server Setup
+//Dim Mailer, strMsgHeader, qryItem, strMsgInfo
+//Dim strFromName, strFromAdd, strMailHost, strRecipientName, strRecipientEmail, strSubject
+$strFromName = $strSiteName;
+$strFromAdd = $strAdminEmail;
+$strMailHost = "";
+$strRecipientName = "Trippnology";
+$strRecipientEmail = $strAdminEmail;
+$strSubject = "Contact from your website";
+$strMsgHeader = "Someone has sent you a message:";
+
+// Set fields to use here
+//DIM strName, strEmail, strTelephone, strMessage
+$strName = request.form("Name");
+$strEmail = request.form("Email");
+$strTelephone = request.form("Telephone");
+$strMessage = request.form("Message");
+?>
