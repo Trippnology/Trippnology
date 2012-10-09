dd3ca7aa42a797edb549ddfce77cb13b77f73eb1
Rikki Tripp <mail@trippnology.com>
2012-10-09 17:21:09 +0100

    Updated code in sidebar to php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,9 +1,9 @@
-<aside>
+<aside class="sidebar">
 	<h1>Sidebar</h1>
 	<p>Need some reusable content in a sidebar? No problem, just call it with:</p>
-	<p><code>&lt;!-- #include file=&quot;inc/sidebar.asp&quot; --&gt;</code></p>
+	<p><code>&lt;?php include &quot;inc/sidebar.asp&quot;; ?&gt;</code></p>
 	<p>The button below activates the modal window which is included in every page.</p>
-	<p><a class="btn" data-toggle="modal" href="#globalmodal">Launch Modal</a><p>
+	<p><a class="btn btn-large" data-toggle="modal" href="#globalmodal">Launch Modal</a><p>
 	<p>The image below is being dynamically resized by CSS.</p>
-	<img class="roundsm" src="http://placekitten.com/250/250" width="250" height="250" alt="Just a placeholder image">
+	<img class="roundsm" src="http://placekitten.com/300/300" width="300" height="300" alt="Just a placeholder image">
 </aside>
