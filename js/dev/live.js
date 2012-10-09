5d8232ded10e4295a35bb5c09b5c8a8b3b66ba4a
Rikki Tripp <mail@trippnology.com>
2012-10-09 01:00:17 +0100

    Updated Live.js to latest version (v4)

--- js/dev/live.js
+++ js/dev/live.js
@@ -1,70 +1,92 @@
 /*
-  Live.js - One script closer to Designing in the Browser - REMOVE FOR PRODUCTION
-  Written for Quplo.com by Martin Kool (@mrtnkl).
+  Live.js - One script closer to Designing in the Browser
+  Written for Handcraft.com by Martin Kool (@mrtnkl).
+
+  Version 4.
+  Recent change: Made stylesheet and mimetype checks case insensitive.
+
   http://livejs.com
-  http://livejs.com/license (MIT)
+  http://livejs.com/license (MIT)  
+  @livejs
+
+  Include live.js#css to monitor css changes only.
+  Include live.js#js to monitor js changes only.
+  Include live.js#html to monitor html changes only.
+  Mix and match to monitor a preferred combination such as live.js#html,css  
+
+  By default, just include live.js to monitor all css, js and html changes.
+  
+  Live.js can also be loaded as a bookmarklet. It is best to only use it for CSS then,
+  as a page reload due to a change in html or css would not re-include the bookmarklet.
+  To monitor CSS and be notified that it has loaded, include it as: live.js#css,notify
 */
 (function () {
 
   var headers = { "Etag": 1, "Last-Modified": 1, "Content-Length": 1, "Content-Type": 1 },
-      Resources = {},
-      CurrentLinkElements = {},
-      OldLinkElements = {},
+      resources = {},
+      pendingRequests = {},
+      currentLinkElements = {},
+      oldLinkElements = {},
       interval = 1000,
       loaded = false,
-      active = false;
+      active = { "html": 1, "css": 1, "js": 1 };
 
   var Live = {
 
     // performs a cycle per interval
-    heartbeat: function () {
-      // check if #live is found in the hash, and activate or deactivate accordingly
-      active = document.location.hash.indexOf("nolive") == -1;
-      if (active) {
+    heartbeat: function () {      
+      if (document.body) {        
         // make sure all resources are loaded on first activation
-        if (!loaded) Live.loadResources();
+        if (!loaded) Live.loadresources();
         Live.checkForChanges();
       }
       setTimeout(Live.heartbeat, interval);
     },
 
     // loads all local css and js resources upon first activation
-    loadResources: function () {
+    loadresources: function () {
 
       // helper method to assert if a given url is local
       function isLocal(url) {
         var loc = document.location,
-            reg = new RegExp("^\\.|^\/|^" + loc.protocol + "//" + loc.host);
-        return url.match(reg) || !url.match(/:\/\//);
+            reg = new RegExp("^\\.|^\/(?!\/)|^[\\w]((?!://).)*$|" + loc.protocol + "//" + loc.host);
+        return url.match(reg);
       }
 
       // gather all resources
       var scripts = document.getElementsByTagName("script"),
           links = document.getElementsByTagName("link"),
-          resources = [document.location.href];
-
-      // track local css urls
-      for (var i = 0; i < links.length; i++) {
-        var link = links[i], type = href = link.getAttribute("type"), href = link.getAttribute("href");
-        if (type && href && type.toLowerCase() == "text/css" && isLocal(href)) {
-          resources.push(href);
-          CurrentLinkElements[href] = link;
-        }
-      }
+          uris = [];
 
       // track local js urls
       for (var i = 0; i < scripts.length; i++) {
         var script = scripts[i], src = script.getAttribute("src");
-        if (src && isLocal(src)) {
-          resources.push(src);
+        if (src && isLocal(src))
+          uris.push(src);
+        if (src && src.match(/\blive.js#/)) {
+          for (var type in active)
+            active[type] = src.match("[#,|]" + type) != null
+          if (src.match("notify")) 
+            alert("Live.js is loaded.");
+        }
+      }
+      if (!active.js) uris = [];
+      if (active.html) uris.push(document.location.href);
+
+      // track local css urls
+      for (var i = 0; i < links.length && active.css; i++) {
+        var link = links[i], rel = link.getAttribute("rel"), href = link.getAttribute("href", 2);
+        if (href && rel && rel.match(new RegExp("stylesheet", "i")) && isLocal(href)) {
+          uris.push(href);
+          currentLinkElements[href] = link;
         }
       }
 
       // initialize the resources info
-      for (var i = 0; i < resources.length; i++) {
-        var url = resources[i];
+      for (var i = 0; i < uris.length; i++) {
+        var url = uris[i];
         Live.getHead(url, function (url, info) {
-          Resources[url] = info;
+          resources[url] = info;
         });
       }
 
@@ -72,29 +94,32 @@
       var head = document.getElementsByTagName("head")[0],
           style = document.createElement("style"),
           rule = "transition: all .3s ease-out;"
-          css = [".livejs-loading * { ",rule, " -webkit-", rule, "-moz-", rule, "-o-", rule, "}"].join('');  
+      css = [".livejs-loading * { ", rule, " -webkit-", rule, "-moz-", rule, "-o-", rule, "}"].join('');
       style.setAttribute("type", "text/css");
       head.appendChild(style);
-      style.styleSheet? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
-      
+      style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
+
       // yep
       loaded = true;
     },
 
     // check all tracking resources for changes
     checkForChanges: function () {
-      for (var url in Resources) {
+      for (var url in resources) {
+        if (pendingRequests[url])
+          continue;
+
         Live.getHead(url, function (url, newInfo) {
-          var oldInfo = Resources[url],
+          var oldInfo = resources[url],
               hasChanged = false;
-          Resources[url] = newInfo;
+          resources[url] = newInfo;
           for (var header in oldInfo) {
             // do verification based on the header type
             var oldValue = oldInfo[header],
                 newValue = newInfo[header],
                 contentType = newInfo["Content-Type"];
-            switch (header) {
-              case "Etag":
+            switch (header.toLowerCase()) {
+              case "etag":
                 if (!newValue) break;
                 // fall through to default
               default:
@@ -113,10 +138,10 @@
 
     // act upon a changed url of certain content type
     refreshResource: function (url, type) {
-      switch (type) {
-        // css files can be reloaded dynamically by replacing the link element       
+      switch (type.toLowerCase()) {
+        // css files can be reloaded dynamically by replacing the link element                               
         case "text/css":
-          var link = CurrentLinkElements[url],
+          var link = currentLinkElements[url],
               html = document.body.parentNode,
               head = link.parentNode,
               next = link.nextSibling,
@@ -127,14 +152,14 @@
           newLink.setAttribute("rel", "stylesheet");
           newLink.setAttribute("href", url + "?now=" + new Date() * 1);
           next ? head.insertBefore(newLink, next) : head.appendChild(newLink);
-          CurrentLinkElements[url] = newLink;
-          OldLinkElements[url] = link;
+          currentLinkElements[url] = newLink;
+          oldLinkElements[url] = link;
 
           // schedule removal of the old link
-          Live.removeOldLinkElements();
+          Live.removeoldLinkElements();
           break;
 
-        // check if an html resource is our current url, then reload       
+        // check if an html resource is our current url, then reload                               
         case "text/html":
           if (url != document.location.href)
             return;
@@ -148,43 +173,45 @@
     },
 
     // removes the old stylesheet rules only once the new one has finished loading
-    removeOldLinkElements: function () {
+    removeoldLinkElements: function () {
       var pending = 0;
-      for (var url in OldLinkElements) {
+      for (var url in oldLinkElements) {
         // if this sheet has any cssRules, delete the old link
         try {
-          var link = CurrentLinkElements[url],
-              oldLink = OldLinkElements[url],
+          var link = currentLinkElements[url],
+              oldLink = oldLinkElements[url],
               html = document.body.parentNode,
               sheet = link.sheet || link.styleSheet,
               rules = sheet.rules || sheet.cssRules;
           if (rules.length >= 0) {
             oldLink.parentNode.removeChild(oldLink);
-            delete OldLinkElements[url];
-            setTimeout(function() {
+            delete oldLinkElements[url];
+            setTimeout(function () {
               html.className = html.className.replace(/\s*livejs\-loading/gi, '');
             }, 100);
           }
         } catch (e) {
           pending++;
         }
-        if (pending) setTimeout(Live.removeOldLinkElements, 50);
+        if (pending) setTimeout(Live.removeoldLinkElements, 50);
       }
     },
 
     // performs a HEAD request and passes the header info to the given callback
     getHead: function (url, callback) {
+      pendingRequests[url] = true;
       var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XmlHttp");
       xhr.open("HEAD", url, true);
       xhr.onreadystatechange = function () {
+        delete pendingRequests[url];
         if (xhr.readyState == 4 && xhr.status != 304) {
           xhr.getAllResponseHeaders();
           var info = {};
           for (var h in headers) {
             var value = xhr.getResponseHeader(h);
             // adjust the simple Etag variant to match on its significant part
-            if (h == "Etag" && value) value = value.replace(/^W\//, '');
-            if (h == "Content-Type" && value) value = value.replace(/^(.*?);.*?$/i, "$1");
+            if (h.toLowerCase() == "etag" && value) value = value.replace(/^W\//, '');
+            if (h.toLowerCase() == "content-type" && value) value = value.replace(/^(.*?);.*?$/i, "$1");
             info[h] = value;
           }
           callback(url, info);
@@ -195,7 +222,12 @@
   };
 
   // start listening
-  window.addEventListener ? window.addEventListener("load", Live.heartbeat, false) :
-                           window.attachEvent("onload", Live.heartbeat, false);
+  if (document.location.protocol != "file:") {
+    if (!window.liveJsLoaded)
+      Live.heartbeat();
+
+    window.liveJsLoaded = true;
+  }
+  else if (window.console)
+    console.log("Live.js doesn't support the file protocol. It needs http.");    
 })();
-/* ################################################################################End of LIVE.JS  */
