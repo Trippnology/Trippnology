bb02fde127117f41ed882e187fbb2d42b4e4ad4c
Rikki Tripp <mail@trippnology.com>
2012-10-13 01:48:08 +0100

    Added blank template.

---
+++ template-blank.php
@@ -0,0 +1,38 @@
+<?php 
+include "inc/settings.php";
+
+$strPageName = "Blank Page";
+$strCanonical = "template-blank.php";
+$strDescription = "Bare minimum of components.";
+$strKeywords = "keyword1, keyword2, keyword3";
+$strTweetText = $strPageName;
+
+include "head.php";
+?>
+
+<body>
+	<?php include "inc/navbar.php"; ?>
+	<div class="container">
+		
+		<div class="row">
+			<div class="span8 offset2" id="main" role="main">
+				<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?php $strPageName ?></a></h1>
+				<article>
+					<p>Your content here.</p>
+				</article>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="span12">
+			<?php include "inc/footer.php"; ?>
+			</div>
+		</div>
+    </div>
+<?php include "scriptblock.php"; ?>
+
+<!-- Put page specific script stuff here -->
+
+<!-- End of page specific script stuff -->
+</body>
+</html>
