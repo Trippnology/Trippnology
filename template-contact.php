ae1cca967cac5a288f9274a53430afad6e456732
Rikki Tripp <mail@trippnology.com>
2012-10-13 02:06:06 +0100

    Added template contact page. Form validation and handling still TODO.

---
+++ template-contact.php
@@ -0,0 +1,69 @@
+<?php 
+include "inc/settings.php";
+
+$strPageName = "Contact Us";
+$strCanonical = "template-contact.php";
+$strDescription = "An example contact page with microdata.";
+$strKeywords = "keyword1, keyword2, keyword3";
+$strTweetText = $strPageName;
+
+include "head.php";
+?>
+
+<body>
+	<?php include "inc/navbar.php"; ?>
+	<div class="container">
+		
+		<div class="row">
+			<div class="span7 offset1" id="main" role="main">
+				<article class="well">
+					<h1 id="pagetitle"><a href="<?=strCanonical?>" title="<?=strPageName?>"><?=strPageName?></a></h1>
+					<img src="http://placekitten.com/128/200/" width="128" height="200" class="right roundsm" alt="Contact <?=strSiteName?>">
+					<section itemscope itemtype="http://schema.org/LocalBusiness">
+						<h2><span itemprop="name"><?=strSiteName?></span></h2>
+						<p><span itemprop="description"><?=strTagline?></span></p>
+		                <p>Email: <span itemprop="email">admin<a href="#" title="Replace this with the usual @ symbol. This is just to stop us getting lots of spam email." class="hastooltip">[at]</a>example.com</span></p>
+						<p>Phone: <span itemprop="telephone"><a href="tel:+44-1234-567890">01234 567890</a></span></p>
+						<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+							<p><span itemprop="streetAddress">1 Example Road</span><br>
+							<span itemprop="addressLocality">Town</span><br>
+							<span itemprop="addressRegion">County</span><br>
+							<span itemprop="postalCode">AB1 2CD</span></p>
+						</div>
+						<p><a itemprop="url" href="<?=strDomain?>"><?=strDomain?></a></p>
+				    </section>
+	                <p><a href="http://twitter.com/<?=strTwitterName?>" rel="external" title="Follow <?=strSiteName?> on twitter">Twitter</a> - <a href="<?=strFacebookURL?>" rel="external" title="Like <?=strSiteName?> on Facebook">Facebook</a></p>
+				</article>
+			</div>
+			<div class="span4">
+				<article class="well">
+					<form action="formtoemail.php" method="post" id="contactForm" class="form-vertical">
+						<label for="Name">Name</label>
+						<input name="Name" id="Name" type="text" class="required">
+						<label for="Email">Email</label>
+						<input name="Email" id="Email" type="email" class="required email">
+						<label for="Telephone">Telephone</label>
+						<input name="Telephone" id="Telephone" type="text">
+						<label for="Message">Your Message</label>
+						<textarea name="Message" id="Message" cols="45" rows="5" class="required"></textarea>
+						<br>
+						<input id="submit" type="submit" value="Send" class="btn btn-success">
+					</form>
+				</article>
+			</div>
+
+		</div>
+		
+		<div class="row">
+			<div class="span12">
+			<?php include "inc/footer.php"; ?>
+			</div>
+		</div>
+    </div>
+<?php include "scriptblock.php"; ?>
+
+<!-- Put page specific script stuff here -->
+
+<!-- End of page specific script stuff -->
+</body>
+</html>
