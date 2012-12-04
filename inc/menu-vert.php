a116f8a72ed3385b68ea2972b0b041d0f16d592a
Rikki Tripp <mail@trippnology.com>
2012-12-04 21:58:06 +0000

    Updated Bootstrap documentation links.

--- inc/menu-vert.php
+++ inc/menu-vert.php
@@ -2,13 +2,12 @@
     <ul class="nav nav-list">
 		<li class="nav-header">Bootstrap:
 		<li><a href="bs/docs/" target="_blank" class="tooltip-right" title="An overview of what is included in Bootstrap"><i class="icon-info-sign"></i> Overview</a>
+		<li><a href="bs/docs/getting-started.html" target="_blank" class="tooltip-right" title="We'll focus on getting started with the compiled Bootstrap files."><i class="icon-info-sign"></i> Getting started</a></li>
 		<li><a href="bs/docs/scaffolding.html" target="_blank" class="tooltip-right" title="Bootstrap is built on a responsive 12-column grid."><i class="icon-info-sign"></i> Scaffolding</a>
 		<li><a href="bs/docs/base-css.html" target="_blank" class="tooltip-right" title="On top of the scaffolding, basic HTML elements are styled and enhanced with extensible classes to provide a fresh, consistent look and feel."><i class="icon-info-sign"></i> Base CSS</a>
 		<li><a href="bs/docs/components.html" target="_blank" class="tooltip-right" title="Dozens of reusable components are built into Bootstrap to provide navigation, alerts, popovers, and much more."><i class="icon-info-sign"></i> Components</a>
 		<li><a href="bs/docs/javascript.html" target="_blank" class="tooltip-right" title="Bring Bootstrap's components to life. Now with 12 custom jQuery plugins."><i class="icon-info-sign"></i> Javascript Plugins</a></li>
-		<li><a href="bs/docs/less.html" target="_blank" class="tooltip-right" title="Customize and extend Bootstrap with LESS, a CSS preprocessor, to take advantage of the variables, mixins, and more used to build Bootstrap's CSS."><i class="icon-info-sign"></i> Using Less</a>
-		<li><a href="bs/docs/download.html" target="_blank" class="tooltip-right" title="Download the full repository or customize your entire Bootstrap build by selecting only the components, javascript plugins, and assets you need."><i class="icon-info-sign"></i> Customise</a>
-		<li><a href="bs/docs/examples.html" target="_blank" class="tooltip-right" title="We've included a few basic examples as starting points for your work with Bootstrap. We encourage folks to iterate on these examples and not simply use them as an end result."><i class="icon-info-sign"></i> Examples</a>
+		<li><a href="bs/docs/customize.html" target="_blank" class="tooltip-right" title="Download the full repository or customize your entire Bootstrap build by selecting only the components, javascript plugins, and assets you need."><i class="icon-info-sign"></i> Customise</a>
 		
 		<li class="nav-header">Demos
         <li><a href="demo-blockfader.php" class="tooltip-right" title="Blockfader jQuery plugin"><i class="icon-play"></i> Blockfader</a>
