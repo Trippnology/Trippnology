1c33f28efce7da7715ba0c07adfbbf9e10739810
Rikki Tripp <mail@trippnology.com>
2012-12-04 20:18:29 +0000

    Start page now displays readme.md (no need to maintain 2 docs).

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-#BootstrASP ported to PHP v0.1
+#BootstraPHP v0.1
+
+##About
+
+The idea is to use Bootstrap and PHP to build a framework of modules
+used for rapid development of web projects. Goals are to make it
+flexible (as in, useful in many senarios), responsive (as in RWD), standards
+compliant (where practical), and ensure it contains everything the
+modern website needs right out of the box.
+
+There will be some components that aren’t needed but it is easy to
+remove what’s not required as things are laid out nice and logically.
+
+Please report bugs via the [Github issue tracker](https://github.com/Trippnology/BootstraPHP/issues) or better still, fix
+them and send a pull request! Likewise if you have suggestions for
+improvement, use the issue tracker or send a pull request.
+
+BootstraPHP is licenced under [CC BY-SA 2.0](https://creativecommons.org/licenses/by-sa/2.0/uk/) which means you are free
+to copy, distribute, hack, slice, make derivatives and even to make
+commercial use of it provided that you both credit [Trippnology](http://www.trippnology.com/) and
+release your work under a licence identical to this one.
+
+Please be aware that some of the components of Bootstrasp are covered by
+different licenses which are listed below.
+
+###What’s included
+
+Submodules:
+
+-   [Bootstrap](https://github.com/twitter/bootstrap.git) ([Apache](https://github.com/twitter/bootstrap/blob/master/LICENSE))
+-   [H5BP Ant Build Script](https://github.com/h5bp/ant-build-script.git) ([MIT](https://github.com/h5bp/ant-build-script/blob/master/MIT-license.txt))
+
+CSS:
+
+-   [Bootswatch](http://bootswatch.com/) ([Apache](http://www.apache.org/licenses/LICENSE-2.0.html))
+-   [Google Fonts](http://www.google.com/webfonts)
+
+Javascript:
+
+-   [Modernizr](https://github.com/Modernizr/Modernizr.git) ([MIT](http://modernizr.com/license/))
+-   [Google Analytics](http://www.google.com/analytics/)
+-   [jQuery]() ([GPL](https://www.gnu.org/licenses/gpl-3.0.html))
+-   [Showdown](https://github.com/coreyti/showdown) ([Copyright John Fraser](https://github.com/coreyti/showdown/blob/master/license.txt))
+
+Social:
+
+-   [Twitter tweet button](http://twitter.com/about/resources/buttons)
+-   [Facebook like button](http://developers.facebook.com/docs/reference/plugins/like/)
+
+Development:
+
+-   [Console.log Wrapper](https://github.com/cpatik/console.log-wrapper.git) ([MIT](https://github.com/cpatik/console.log-wrapper/blob/master/LICENSE))
+-   [Live.js](http://livejs.com/) ([MIT](http://livejs.com/license))
--- start.php
+++ start.php
@@ -24,59 +24,8 @@ include "head.php";
 			
 			<div class="span7" id="main" role="main">
 				<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?php $strPageName ?></a></h1>
-				<article>
+				<article class="readme">
 					<img src="img/trippnology-logo-square-180.png" alt="Trippnology square logo" class="right roundlrg">
-					<h1>About</h1>
-					<p>The idea is to use Bootstrap and PHP to build a framework of modules used for rapid development of web projects. We're trying to make it flexible (as in, useful in many senarios), responsive, standards compliant (where practical), and ensure it contains everything the modern website needs right out of the box.</p>
-					<p>There will be some components that aren't needed but it is easy to remove what's not required as things are laid out nice and logically.</p>
-					<p>Full details are included in the <a href="#modal-readme" class="tooltip-top" data-toggle="modal" title="Read the fricken manual">Readme file</a>.</p>
-					<p>Please report bugs via the <a href="https://github.com/Trippnology/BootstraPHP/issues" title="BootstraPHP Issues">Github issue tracker</a> or better still, fix them and send a pull request! Likewise if you have suggestions for improvement, send a pull request.</p>
-					<p>BootstrPHP is licenced under <a class="label tooltip-top" rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/2.0/uk/" title="Attribution-ShareAlike 2.0 UK: England &amp; Wales">CC BY-SA 2.0</a> which means you are free to copy, distribute, hack, slice, make derivatives and even to make commercial use of it provided that you both credit <a rel="external" href="http://www.trippnology.com/">Trippnology</a> and release your work under a licence identical to this one.</p>
-					<p>Please be aware that some of the components of Bootstrasp are covered by different licenses which are listed below.</p>
-				</article>
-
-				<article>
-					<h1>What's included</h1>
-					<p>Submodules:</p>
-					<ul>
-						<li><a rel="external" href="https://github.com/twitter/bootstrap.git">Bootstrap</a> <a class="label" rel="external" href="https://github.com/twitter/bootstrap/blob/master/LICENSE">Apache</a>
-						<li><a rel="external" href="https://github.com/Modernizr/Modernizr.git">Modernizr</a> <a class="label" rel="external" href="http://modernizr.com/license/">MIT</a>
-						<!-- <li><a rel="external" href="https://github.com/davatron5000/FitText.js.git">FitText</a> <a class="label" rel="external" href="http://sam.zoy.org/wtfpl/">WTFPL</a> -->
-						<li><a rel="external" href="https://github.com/h5bp/ant-build-script.git">H5BP Ant Build Script</a>
-					</ul>
-
-					<p>CSS</p>
-					<ul>
-						<li><a rel="external" href="http://bootswatch.com/">Bootswatch</a> <a class="label" rel="external" href="http://www.apache.org/licenses/LICENSE-2.0.html">Apache</a>
-						<li><a rel="external" href="http://www.google.com/webfonts">Google Fonts</a>
-					</ul>
-					<p>Javascript:</p>
-					<ul>
-						<li><a rel="external" href="http://jquery.com/">jQuery</a> <a class="label" rel="external" href="https://www.gnu.org/licenses/gpl-3.0.html">GPL</a> and these plugins:
-							<ul>
-								<!-- <li><a rel="external" href="http://gsgd.co.uk/sandbox/jquery/easing/">jQuery Easing</a>
-								<li><a rel="external" href="http://archive.plugins.jquery.com/project/mousewheel">jQuery Mousewheel</a> <a class="label" rel="external" href="https://github.com/brandonaaron/jquery-mousewheel/blob/master/LICENSE.txt">&copy;Brandon Aaron</a>
-								<li><a rel="external" href="http://bassistance.de/jquery-plugins/jquery-plugin-validation/">jQuery Validation</a> <a class="label" rel="external" href="https://www.gnu.org/licenses/gpl-3.0.html">GPL</a>
-								<li><a rel="external" href="http://www.codefocus.ca/goodies/blockfader">BlockFader</a> <a class="label" rel="external" href="https://www.gnu.org/licenses/gpl-3.0.html">GPL</a>
-								<li><a rel="external" href="http://fancybox.net/">Fancybox</a> <a class="label" rel="external" href="https://www.gnu.org/licenses/gpl-3.0.html">GPL</a>
-								<li>ExternalLinks -->
-							</ul>
-						</li>
-						<li><a rel="external" href="http://www.google.com/analytics/">Google Analytics</a>
-					</ul>
-
-					<p>Social:</p>
-					<ul>
-						<!-- <li><a rel="external" href="http://twitter.com/about/resources/buttons">Twitter Tweet Button</a>
-						<li><a rel="external" href="http://developers.facebook.com/docs/reference/plugins/like/">Facebook Like Button</a>
-						<li><a rel="external" href="http://www.addthis.com/">AddThis Button</a> -->
-					</ul>
-
-					<p>Development</p>
-					<ul>
-						<li><a rel="external" href="http://livejs.com/">Live.js</a> <a class="label" rel="external" href="http://livejs.com/license">MIT</a>
-					</ul>
-
 				</article>
 			</div>
 			
@@ -94,7 +43,14 @@ include "head.php";
 <?php include "scriptblock.php"; ?>
 
 <!-- Put page specific script stuff here -->
-<?php include "inc/modal-readme.php" ?>
+<script src="js/libs/showdown.js"></script>
+<script>
+	jQuery.get('README.md', function(md) {
+		var converter = new Showdown.converter();
+		var mdhtml = converter.makeHtml(md);
+		$(".readme").append(mdhtml);
+	});
+</script>
 <!-- End of page specific script stuff -->
 </body>
 </html>
