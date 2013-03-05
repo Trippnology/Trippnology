d14b9a997dc154554cc7b65ad13acc82268202cb
Trippnology <mail@trippnology.com>
2013-03-05 15:43:37 +0000

    Updated nav links.

--- inc/menu-vert.php
+++ inc/menu-vert.php
@@ -1,16 +1,20 @@
-<nav class="vert-menu collapse" id="menu-vert">
-    <ul class="nav nav-list">
-		<li class="nav-header">Bootstrap:
-		<li><a href="bs/docs/" target="_blank" class="tooltip-right" title="An overview of what is included in Bootstrap"><i class="icon-info-sign"></i> Overview</a>
-		<li><a href="bs/docs/getting-started.html" target="_blank" class="tooltip-right" title="We'll focus on getting started with the compiled Bootstrap files."><i class="icon-info-sign"></i> Getting started</a></li>
-		<li><a href="bs/docs/scaffolding.html" target="_blank" class="tooltip-right" title="Bootstrap is built on a responsive 12-column grid."><i class="icon-info-sign"></i> Scaffolding</a>
-		<li><a href="bs/docs/base-css.html" target="_blank" class="tooltip-right" title="On top of the scaffolding, basic HTML elements are styled and enhanced with extensible classes to provide a fresh, consistent look and feel."><i class="icon-info-sign"></i> Base CSS</a>
-		<li><a href="bs/docs/components.html" target="_blank" class="tooltip-right" title="Dozens of reusable components are built into Bootstrap to provide navigation, alerts, popovers, and much more."><i class="icon-info-sign"></i> Components</a>
-		<li><a href="bs/docs/javascript.html" target="_blank" class="tooltip-right" title="Bring Bootstrap's components to life. Now with 12 custom jQuery plugins."><i class="icon-info-sign"></i> Plugins</a></li>
-		<li><a href="bs/docs/customize.html" target="_blank" class="tooltip-right" title="Download the full repository or customize your entire Bootstrap build by selecting only the components, javascript plugins, and assets you need."><i class="icon-info-sign"></i> Customise</a>
-		
-        <li class="nav-header">Example Pages
-        <li><a href="template-blank.php" class="tooltip-right" title="A bare page with just the basic setup options"><i class="icon-th"></i> Blank Page</a>
-        <li><a href="template-contact.php" class="tooltip-right" title="Example contact page with Microdata and a contact form"><i class="icon-th"></i> Contact Page</a>
+<nav class="vert-menu" id="menu-vert">
+    <ul class="nav inline">
+		<li class="nav-header">Services:</li>
+		<li><a href="web-design-attleborough">Web Design</a></li>
+		<li><a href="search-engine-optimisation">SEO</a></li>
+		<li><a href="it-support">IT Support</a></li>
+		<li><a href="social-media">Social Media</a></li>
+<!-- 		<li><a href="computer-repair">Computer Repair</a></li>
+	 -->	</ul>
+	<ul class="nav inline">
+        <li class="nav-header">Site:</li>
+		<li><a href="/">Home</a></li>
+		<li><a href="about">About</a></li>
+		<li><a href="contact">Contact</a></li>
+		<li><a href="prices">Prices</a></li>
+		<li><a href="testimonials">Testimonials</a></li>
+		<li><a href="http://blog.trippnology.com/">Blog</a></li>
+		<li><a href="shed">The Shed</a></li>
     </ul>
 </nav>
