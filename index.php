dc2260536d5a506218e6ee8c654c03492eef62dd
Trippnology <mail@trippnology.com>
2013-03-05 16:59:21 +0000

    Swapped out temp images at bottom of page.

--- index.php
+++ index.php
@@ -117,19 +117,19 @@ include "head.php";
 				<!-- Three columns of text boxes -->
 				<div class="row">
 					<div class="span3">
-						<img class="img-circle" data-src="holder.js/140x140">
+						<img class="img-circle" src="img/home/code.png" width="140" height="140">
 						<h2>Web Design</h2>
 						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
 						<p><a class="btn" href="#">View details »</a></p>
 					</div><!-- /.span4 -->
 					<div class="span3">
-						<img class="img-circle" data-src="holder.js/140x140">
+						<img class="img-circle" src="img/home/graph.png" width="140" height="140">
 						<h2>SEO</h2>
 						<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 						<p><a class="btn" href="#">View details »</a></p>
 					</div><!-- /.span4 -->
 					<div class="span3">
-						<img class="img-circle" data-src="holder.js/140x140">
+						<img class="img-circle border-dark border-fat" src="img/home/it-support.jpg" width="140" height="140">
 						<h2>IT Support</h2>
 						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 						<p><a class="btn" href="#">View details »</a></p>
