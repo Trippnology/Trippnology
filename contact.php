52c196b27c15328f02261a7b692c8d84f002cf2c
Trippnology <mail@trippnology.com>
2013-03-06 12:39:34 +0000

    Corrected contact info.

--- contact.php
+++ contact.php
@@ -2,9 +2,9 @@
 include "inc/settings.php";
 
 $strPageName = "Contact Us";
-$strCanonical = "template-contact.php";
-$strDescription = "An example contact page with microdata.";
-$strKeywords = "keyword1, keyword2, keyword3";
+$strCanonical = "contact";
+$strDescription = "Contact us for high quality Web Design, SEO, IT Support and more.";
+$strKeywords = "trippnology, attleborough, contact, support, technology, design, computer";
 $SEOtitle = $strPageName . " | " . $siteName;
 $pageSlug = "contact";
 $strTweetText = $strPageName;
@@ -13,25 +13,29 @@ include "head.php";
 ?>
 
 <body class="page-<?=$pageSlug?>">
-	<?php include "inc/navbar.php"; ?>
-	<div class="container">
-		
+<!--     <div class="navbar-wrapper">
+    	 -->    	<div class="container">
+			<?php include "inc/navbar.php"; ?>   		
+    	</div>
+<!--     </div> -->
+
+	<div class="container">	
 		<div class="row">
 			<div class="span7 offset1" id="main" role="main">
 				<article class="well">
 					<h1 id="pagetitle"><a href="<?=$strCanonical?>" title="<?=$strPageName?>"><?=$strPageName?></a></h1>
-					<img src="http://placekitten.com/128/200/" width="128" height="200" class="pull-right roundsm" alt="Contact <?=siteName?>">
+					<img src="img/trippnology-logo-square-180.png" width="180" height="180" class="pull-right roundsm" alt="Contact <?=siteName?>">
 					<section itemscope itemtype="http://schema.org/LocalBusiness">
 						<h2><span itemprop="name"><?=$siteName?></span></h2>
 						<p><span itemprop="description"><?=$siteTagline?></span></p>
-		                <p>Email: <span itemprop="email">admin<a href="#" title="Antispam" data-content="Replace this with the usual @ symbol. This is just to stop us getting lots of spam email." class="antispam">[at]</a>example.com</span></p>
-						<p>Phone: <span itemprop="telephone"><a href="tel:+44-1234-567890">01234 567890</a></span></p>
+		                <p>Email: <span itemprop="email">mail<a href="#" title="Antispam" data-content="Replace this with the usual @ symbol. This is just to stop us getting lots of spam email." class="antispam">[at]</a>trippnology.com</span></p>
+						<p>Phone: <span itemprop="telephone"><a href="tel:+44-1953-451231">01953 451231</a></span></p>
 						<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-							<p><span itemprop="streetAddress">1 Example Road</span><br>
-							<span itemprop="addressLocality">Town</span><br>
-							<span itemprop="addressRegion">County</span><br>
-							<span itemprop="postalCode">AB1 2CD</span><br>
-							<a href="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=trippnology&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=10.921321,27.13623&amp;ie=UTF8&amp;hq=trippnology&amp;hnear=&amp;t=m&amp;cid=17370591561013489825&amp;z=13&amp;iwloc=A&amp;output=embed" class="badge popup-map"><i class="icon-globe"></i> Map</a>
+							<p><span itemprop="streetAddress">Besthorpe Lodge</span><br>
+							<span itemprop="addressLocality">Attleborough</span><br>
+							<span itemprop="addressRegion">Norfolk</span><br>
+							<span itemprop="postalCode">NR17 2LJ</span><br>
+							<a href="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=trippnology&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=10.921321,27.13623&amp;ie=UTF8&amp;hq=trippnology&amp;hnear=&amp;t=m&amp;cid=17370591561013489825&amp;z=13&amp;iwloc=A&amp;output=embed" class="badge popup-map"><i class="icon-map-marker"></i> Map</a>
 							</p>
 						</div>
 						<p><a itemprop="url" href="<?=$siteDomain?>"><?=$siteDomain?></a></p>
