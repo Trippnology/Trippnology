7efb607db1ff0e75e15ac3e82ee658d62ea5aaac
Trippnology <mail@trippnology.com>
2013-03-06 11:16:22 +0000

    Added rewrite rules for pretty URLs.

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,17 +1,16 @@
 <div class="navbar">
 	<div class="navbar-inner roundlrg">
 		<div class="container">
-			<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
 			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">Menu</a>
 			<a class="brand" href="<?=$siteDomain?>" title="<?=$siteName?>"><?=$siteName?></a>
 			<div class="nav-collapse">
 				<ul class="nav">
-					<li><a href="web-design-attleborough/">Web Design</a></li>
-					<li><a href="search-engine-optimisation-attleborough/">SEO</a></li>
-					<li><a href="it-support-attleborough/">IT Support</a></li>
+					<li><a href="web-design-attleborough">Web Design</a></li>
+					<li><a href="search-engine-optimisation-attleborough">SEO</a></li>
+					<li><a href="it-support-attleborough">IT Support</a></li>
 					<li><a href="http://blog.trippnology.com/">Blog</a></li>
-					<li><a href="shed/">The Shed</a></li>
-					<li><a href="contact/">Contact</a></li>
+					<li><a href="shed">The Shed</a></li>
+					<li><a href="contact">Contact</a></li>
 				</ul>
 			</div>
 		</div>
