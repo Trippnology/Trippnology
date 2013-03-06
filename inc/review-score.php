98df9c7145e69a2782106f7fedfb9110e663f9a1
Trippnology <mail@trippnology.com>
2013-03-06 15:38:31 +0000

    Added include file with review microdata.

---
+++ inc/review-score.php
@@ -0,0 +1,6 @@
+<div itemtype="http://data-vocabulary.org/Review-aggregate" itemscope="">
+	<span itemprop="itemreviewed">Trippnology</span> is rated 
+	<span itemtype="http://data-vocabulary.org/Rating" itemscope="" itemprop="rating">
+		<span itemprop="average">5</span> out of <span itemprop="best">5</span>  based on <span itemprop="votes">4</span> ratings.
+	</span>
+</div>
