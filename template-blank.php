745b9dd4db610337876bd3ded8ccae904f62d843
Trippnology <mail@trippnology.com>
2013-03-06 23:57:16 +0000

    Added page header and new settings.

--- template-blank.php
+++ template-blank.php
@@ -7,6 +7,8 @@ $strDescription = "Bare minimum of components.";
 $strKeywords = "keyword1, keyword2, keyword3";
 $SEOtitle = $strPageName . " | " . $siteName;
 $pageSlug = "blank";
+$pageHeaderImageURL = "img/home/4150657215_a411d19e5e_b.jpg";
+$pageHeaderImageURLSource = "https://secure.flickr.com/photos/manchester-monkey/4150657215/";
 $strTweetText = $strPageName;
 
 include "head.php";
@@ -14,6 +16,17 @@ include "head.php";
 
 <body class="page-<?=$pageSlug?>">
 	<?php include "inc/navbar.php"; ?>
+
+	<header class="page-header">
+		<img src="<?=$pageHeaderImageURL?>" alt="" data-sourceurl="<?=$pageHeaderImageURLSource?>">
+		<div class="container">
+			<div class="header-caption roundlrg">
+				<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?= $strPageName ?></a></h1>
+				<h2>You know your business. We know the web. Let's get together and make something great!</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident laudantium aliquam ullam sequi ipsa aperiam mollitia accusantium consequatur temporibus non ad aspernatur alias incidunt possimus maiores veritatis neque harum atque.</p>
+			</div>
+		</div>
+	</header>
 
 	<div class="container">	
 		<div class="row">
