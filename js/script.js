a5b3ab0fd58c9a2f88075821dbb3a580ddc8efbf
Trippnology <mail@trippnology.com>
2013-03-07 13:20:55 +0000

    Removed 3d link effect from buttons.

--- js/script.js
+++ js/script.js
@@ -49,8 +49,8 @@ $(document).ready(function() {
 		});
 	};
 	initRollLinks();
-	// Dump roll links if they are in a badge
-	$('a.badge').removeClass('roll-link');
+	// Dump roll links if they are in a badge or button
+	$('a.badge, a.btn, button.btn').removeClass('roll-link');
 
 	// Smooth scroll to anchors on the page
 	var scroller = function(id){
