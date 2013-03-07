3d5178174ee18a371fcdbcdb51b758c544116014
Trippnology <mail@trippnology.com>
2013-03-07 12:51:08 +0000

    Some content changes.

--- about.php
+++ about.php
@@ -20,16 +20,18 @@ include "head.php";
 			<div class="span9" id="main" role="main">
 				<article>
 					<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?= $strPageName ?></a></h1>
+					<h2>Hello there! Nice to meet you!</h2>
+					<p class="lead">Here's a little explanation of what we're all about:</p>
 					<div class="span4">
 						<ol>
-							<li><span class="lead">Useful is forever</span><br>
-							Bells and whistles wear off, but usefulness never does. We build useful software that does just what you need and nothing you don’t.</li>
 							<li><span class="lead">Great service is everything</span><br>
 							We’re famous for fast and friendly customer service. We work hard to make sure we live up to that reputation every day. <a href="testimonials">See how our customers rate our service</a>.</li>
 							<li><span class="lead">Clarity is king</span><br>
-							Buzzwords, lingo, and sensationalized sales-and-marketing-speak have no place at 37signals. We communicate clearly and honestly.</li>
+							Buzzwords, lingo, and sensationalized sales-and-marketing-speak have no place at Trippnology. We communicate clearly and honestly.</li>
 							<li><span class="lead">Our customers are our investors</span><br>
-							Our customers fund our daily operations by paying for our products. We answer to them — not investors, the stock market, or a board of directors.</li>
+							Our customers fund our daily operations by paying for our services. We answer to them — not investors, the stock market, or a board of directors.</li>
+							<li><span class="lead">Green through and through</span><br>
+							We run a paperless office and use a combination of public transport and human power for all our travels meaning our carbon footprint is tiny.</li>
 						</ol>
 					</div>
 					<div class="span4">
@@ -37,7 +39,7 @@ include "head.php";
 							<li><span class="lead">The basics are beautiful</span><br>
 							We’ll never overlook what really matters: The basics. Great service, ease of use, honest pricing, and respect for our customer’s time, money, and trust.</li>
 							<li><span class="lead">No hidden fees or secret prices</span><br>
-							We believe everyone is entitled to the best price we can offer. Our prices are public, published right on our site, and the same no matter who you are.</li>
+							We believe everyone is entitled to the best price we can offer. Our prices are public, <a href="prices">published right on our site</a>, and the same no matter who you are.</li>
 							<li><span class="lead">Software should be easy</span><br>
 							Our products are intuitive. You’ll pick them up in seconds or minutes, not hours, days or weeks. We don’t sell you training because you don’t need it.</li>
 							<li><span class="lead">Long-term contracts are obscene</span><br>
