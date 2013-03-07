58f375bf181c1e751d2a7eeefe2625a0537166a2
Trippnology <mail@trippnology.com>
2013-03-07 17:53:29 +0000

    Some content and formatting changes.

--- web-design-attleborough.php
+++ web-design-attleborough.php
@@ -58,9 +58,13 @@ include "head.php";
 
 				<article id="project-assessment">
 					<h1>Project Assessment</h1>
-					<p>Before we can build you a website, we need to know what it is that you have in mind. The only way to give you an accurate quote is to understand what your needs are and what you are trying to achieve. You may not know this yourself initially but we'll talk you through it and help you set appropiate goals.</p>
+					<div class="well white span2 pull-right">
+						<p><span class="lead">Bottom line:</span> We assess your idea and give you a plan to make it a reality. If you go ahead with the project, this service is free of charge.</p>
+						<p><a href="contact" class="btn btn-mini btn-success">Let's talk »</a></p>
+					</div>
 					<p>A project assessment, as we’ve defined it, is a detailed plan for the work that is to be done on a project, and explains how we do it. We eliminate the guess work, and detail the project out to such a level that this document becomes a living part of the development process, being referred back to and acting as the guide towards the project’s successful completion.</p>
 					<p>These details are valuable to both you (in making sure you know exactly what you’re getting) and to us (in making sure that we know exactly what we need to deliver).</p>
+					<p>Before we can build you a website, we need to know what it is that you have in mind. The only way to give you an accurate quote is to understand what your needs are and what you are trying to achieve. You may not know this yourself initially but we'll talk you through it and help you set appropiate goals.</p>
 					<p>Putting this together takes time and includes a 1 hour meeting with you (in person or via the web) so we do charge for this. Once you have your completed project assessment, you are under no obligation to go ahead with the build but if you do, the cost of the assessment will be refunded.</p>
 					<p><a href="contact">Contact us</a> today and let's get the ball rolling!</p>
 				</article>
@@ -69,25 +73,101 @@ include "head.php";
 
 				<article id="website-updates">
 					<h1>Website Updates</h1>
+					<div class="well white span2 pull-right">
+						<p><span class="lead">Bottom line:</span> We can bring your current website up to scratch and make it mobile friendly.</p>
+						<p><a href="contact" class="btn btn-mini btn-success">Let's talk »</a></p>
+
+					</div>
 					<p>The landcape of the web is ever changing. New technologies emerge and with hindsight, we sometimes realise that the way we built the web in the past was less than ideal.</p>
-					<p>As users of the web, we're no longer chained to our desks. The use of smartphones and tablets is soaring and many older websites have problems displaying on mobile devices and in some cases, are entirely broken. Here's where we come in. We'll fix up those old pages and make sure your customers aren't frustrated when using your site.</p>
+					<p>As users of the web, we're no longer chained to our desks. The use of smartphones and tablets is soaring and many older websites have problems displaying on mobile devices and in some cases, are entirely broken.</p>
+					<p>Here's where we come in. We'll fix up those old pages and make sure your customers aren't frustrated when using your site. We'll bring your website up to modern standards and make it more accessible to everyone.</p>
 					<p><a href="contact">Contact us</a> today and let's get rid of those cobwebs!</p>
 				</article>
 
 				<hr class="divider roundsm">
 
 				<article id="portfolio">
-					<h1>Portfolio</h1>
+					<div class="row">
+						<div class="span4">
+							<h1>Portfolio</h1>							
+						</div>
+						<div class="well white span5">
+							<p><span class="lead">Key:</span> <i class="icon-tags"></i> = HTML5 | <i class="icon-resize-full"></i> = Responsive | <i class="icon-shopping-cart"></i> = eCommerce</p>
+						</div>
+					</div>
 					<ul class="thumbnails portfolio">
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
-						<li class="span3"><div class="thumbnail roundlrg shadowright"><img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt=""><h4>Title</h4><p>Short description </p><p class="badge">example.com</p></div></li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180/text:Coming Soon" alt="">
+								<h4>Advanced Camera Services</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Avant Tecno</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Headspace Accountancy</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Viper Sounds</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Whiterod Surfacing</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Johnny Watton</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Norwich French Church Charity</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Square Deal Storage</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
+						<li class="span3">
+							<div class="thumbnail roundlrg shadowright">
+								<img class="img-polaroid roundlrg" data-src="holder.js/180x180" alt="">
+								<h4>Glassact</h4>
+								<p>Short description </p>
+								<p><a href="#" class="badge">example.com</a> <span class="badge"><i class="icon-tags"></i></span> <span class="badge"><i class="icon-resize-full"></i></span></p>
+							</div>
+						</li>
 					</ul>
 				</article>
 			</div><!-- /#main -->
