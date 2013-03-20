4e8f40fe4f5c3af68aba5d95862f5d38f3bba42d
Trippnology <mail@trippnology.com>
2013-03-20 15:18:22 +0000

    Added some in page navigation.

--- prices.php
+++ prices.php
@@ -31,9 +31,20 @@ include "head.php";
 	<div class="container">	
 		<div class="row">
 			<div class="span8 offset1" id="main" role="main">
+				<ul class="nav nav-tabs">
+					<li class="active"><a href="#web-design">Web design</a></li>
+					<li><a class="scrollme" href="#seo">SEO</a></li>
+					<li><a class="scrollme" href="#domain-names">Domain names</a></li>
+					<li><a class="scrollme" href="#web-hosting">Web hosting</a></li>
+					<li><a class="scrollme" href="#it-support">IT Support</a></li>
+					<li><a class="scrollme" href="#service-area">Service area</a></li>
+				</ul>
+
 				<article>
-					<h1>Web design and other web services</h1>
+					<h1 id="web-design">Web design and other web services</h1>
 					<p><a href="web-design-attleborough">Web design</a>, updates, ammendments, technical changes etc. - <strong>£35</strong> per hour.</p>
+					<p>Due to it's bespoke nature, <a href="web-design-attleborough">website design</a> quotes are based on your individual requirements. Please <a href="contact">contact us</a> for a chat, we would love to hear your plans.</p>
+					<p>We recommend starting with a <a href="web-design-attleborough#project-assessment">project assessment</a> which includes an hour meeting with you and then a full breakdown of the work to be carried out. This service is billed at £100 but is included free of charge if you go ahead with the project.</p>
 					<p>How much does it cost to make a website?</p>
 					<table class="table price-table-web">
 						<tr>
@@ -52,21 +63,22 @@ include "head.php";
 							<td style="width:20%" class="grey" colspan="2">*A mucky website with videos of muffs, knockers and winkies.</td>
 						</tr>
 					</table>
-					<p>*I've never built one of these, I just threw it in for a bit of spice.</p>
-					<p>Due to it's bespoke nature, <a href="web-design-attleborough">website design</a> quotes are based on your individual requirements. Please <a href="contact">contact us</a> for a chat, we would love to hear your plans.</p>
-					<p>We recommend starting with a <a href="web-design-attleborough#project-assessment">project assessment</a> which includes an hour meeting with you and then a full breakdown of the work to be carried out. This service is billed at £100 but is included free of charge if you go ahead with the project.</p>
-					<h2>Search Engine Optimisation</h2>
+					<p>*We've never built one of these, we just threw it in for a bit of spice.</p>
+					<h2 id="seo">Search Engine Optimisation</h2>
 					<p>All <a href="search-engine-optimisation">SEO</a> work is charged at our standard Web Service rate - <strong>£35</strong> per hour.</p>
-					<h3>Domain name renewals</h3>
+					<h3 id="domain-names">Domain names</h3>
 					<ul class="unstyled">
 						<li>.co.uk - <strong>£20 per 2 years</strong></li>
 						<li>.net/.org/.com - <strong>£20 per 1 year</strong></li>
 						<li>Others - <strong>POA</strong></li>
 					</ul>
+					<h4 id="web-hosting">Web Hosting</h4>
 				</article>
 
+				<hr class="divider roundsm">
+
 				<article>
-					<h1>Ad-hoc IT support and computer repair</h1>
+					<h1 id="it-support">Ad-hoc IT support and computer repair</h1>
 					<div class="well white pull-right span2">
 						<p>While our pricing is good, our customer service is outstanding.</p>
 						<p><a href="testimonials" class="btn btn-mini">Our customers agree!</a></p>
@@ -92,15 +104,15 @@ include "head.php";
 					  Up to 10 - <strong>£250 per month </strong><br>
 		                Up to 20 - <strong>£475 per month </strong><br>
 		                More than 20 - Please <a href="contact">contact us</a>.</p>
-					<h3>Our Service Area</h3>
+					<h3 id="service-area">Our Service Area</h3>
 					<p>We mainly serve <span itemscope itemtype="http://schema.org/Place" itemprop="name">Attleborough</span>, <span itemscope itemtype="http://schema.org/Place" itemprop="name">Wymondham</span>, <span itemscope itemtype="http://schema.org/Place" itemprop="name">Norwich</span> and <span itemscope itemtype="http://schema.org/Place" itemprop="name">Thetford</span> but we also cover surrounding towns and villages. The map below serves as a rough guide.</p>
 		            <div id="googlemap">
 						<iframe width="100%" scrolling="no" height="500" frameborder="0" src="http://maps.google.co.uk/maps?cid=17370591561013489825&amp;ssa=1&amp;ie=UTF8&amp;hq=+loc:+&amp;hnear=&amp;source=embed&amp;ll=52.538703,1.005369&amp;spn=0.292462,0.673827&amp;iwloc=A&amp;output=embed" marginwidth="0" marginheight="0"></iframe>
 		                <p><a target="_blank" href="http://maps.google.co.uk/maps?cid=17370591561013489825&amp;ssa=1&amp;ie=UTF8&amp;hq=+loc:+&amp;hnear=&amp;source=embed&amp;ll=52.538703,1.005369&amp;spn=0.292462,0.673827&amp;iwloc=A">View Larger Map</a></p>
 		            </div>
 		            <h4>Notes</h4>
-					<p><a name="callout">*</a> Callout charges may be higher than stated based on your location - please double check with us first.</p>
-					<p><a name="devices">**</a> The following items count as a Device: Desktop PC (<strong>including</strong> monitor), Laptop, Server, Printer, Scanner, Router, Hub, Wireless Access Point and other similar items. </p>
+					<p id="callout">* Callout charges may be higher than stated based on your location - please double check with us first.</p>
+					<p id="devices">** The following items count as a Device: Desktop PC (<strong>including</strong> monitor), Laptop, Server, Printer, Scanner, Router, Hub, Wireless Access Point and other similar items. </p>
 				</article>
 			</div><!-- /#main -->
 			<div class="span3">
