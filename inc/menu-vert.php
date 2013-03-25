564dc441dd02b100419fb08f5b14781060525f5e
Trippnology <mail@trippnology.com>
2013-03-25 17:24:48 +0000

    Changed hover effect on links in the nav block. Fixes #7

--- inc/menu-vert.php
+++ inc/menu-vert.php
@@ -1,20 +1,20 @@
 <nav class="vert-menu" id="menu-vert">
     <ul class="nav inline">
 		<li class="nav-header">Services:</li>
-		<li><a href="web-design-attleborough">Web Design</a></li>
-		<li><a href="search-engine-optimisation">SEO</a></li>
-		<li><a href="it-support">IT Support</a></li>
-		<li><a href="social-media">Social Media</a></li>
+		<li><a class="vert-menu-item" href="web-design-attleborough">Web Design</a></li>
+		<li><a class="vert-menu-item" href="search-engine-optimisation">SEO</a></li>
+		<li><a class="vert-menu-item" href="it-support">IT Support</a></li>
+		<li><a class="vert-menu-item" href="social-media">Social Media</a></li>
 <!-- 		<li><a href="computer-repair">Computer Repair</a></li>
 	 -->	</ul>
 	<ul class="nav inline">
         <li class="nav-header">Site:</li>
-		<li><a href="/">Home</a></li>
-		<li><a href="about">About</a></li>
-		<li><a href="contact">Contact</a></li>
-		<li><a href="prices">Prices</a></li>
-		<li><a href="testimonials">Testimonials</a></li>
-		<li><a href="http://blog.trippnology.com/">Blog</a></li>
-		<li><a href="shed">The Shed</a></li>
+		<li><a class="vert-menu-item" href="/">Home</a></li>
+		<li><a class="vert-menu-item" href="about">About</a></li>
+		<li><a class="vert-menu-item" href="contact">Contact</a></li>
+		<li><a class="vert-menu-item" href="prices">Prices</a></li>
+		<li><a class="vert-menu-item" href="testimonials">Testimonials</a></li>
+		<li><a class="vert-menu-item" href="http://blog.trippnology.com/">Blog</a></li>
+		<li><a class="vert-menu-item" href="shed">The Shed</a></li>
     </ul>
 </nav>
