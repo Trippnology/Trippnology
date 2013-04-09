8510529fe0208b53ee2d22785b0aedaf92ac445e
Trippnology <mail@trippnology.com>
2013-04-09 01:29:30 +0100

    Automatic whitespace adjustment.

--- inc/share-facebook-like.php
+++ inc/share-facebook-like.php
@@ -1,7 +1,7 @@
 <div id="fb-root"></div>
-<div class="fb-like" 
-data-href="<?=$socialFacebookURL?>" 
-data-send="false" 
-data-layout="button_count" 
-data-width="90" 
+<div class="fb-like"
+data-href="<?=$socialFacebookURL?>"
+data-send="false"
+data-layout="button_count"
+data-width="90"
 data-show-faces="false"></div>
