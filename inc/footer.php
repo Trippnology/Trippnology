0d4c287fba6ceabb7a9311f161e80f23525a6743
Trippnology <mail@trippnology.com>
2013-04-15 21:59:10 +0100

    Updated license info.

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer role="contentinfo">
 	<hr class="divider roundsm">
-	<p>Content &copy;<?php echo date("Y"); ?> <a href="<?=$siteDomain?>" title="<?=$siteName?>"><?=$siteName?></a> | Design by Trippnology <a href="http://www.trippnology.com/web-design-attleborough.asp" rel="external" title="Web Design Attleborough">Web Design Attleborough</a></p>
+	<p><a href="http://creativecommons.org/licenses/by-sa/2.0/uk/" rel="license"><img src="img/ui/cc-by-sa-80x15.png" alt="Creative Commons Attribution-ShareAlike 2.0 UK: England & Wales license"></a> <a href="<?=$siteDomain?>" title="<?=$siteName?>"><?=$siteName?></a></p>
     <p>Follow us on <a href="<?=$socialFacebookURL?>" rel="external" title="<?=$siteName?> Facebook Page">Facebook</a> and <a href="http://twitter.com/<?=$socialTwitterName?>" rel="external" title="<?=$siteName?> on twitter">twitter</a>.</p>
 </footer>
