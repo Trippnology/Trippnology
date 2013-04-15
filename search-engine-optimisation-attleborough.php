fb520b927975ca5770fdda2a83f4f4e46adfe9ee
Trippnology <mail@trippnology.com>
2013-04-16 00:27:42 +0100

    Added content to SEO page.

--- search-engine-optimisation-attleborough.php
+++ search-engine-optimisation-attleborough.php
@@ -22,17 +22,35 @@ include "head.php";
 		<div class="container">
 			<div class="header-caption roundlrg">
 				<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?= $strPageName ?></a></h1>
-				<h2>You know your business. We know the web. Let's get together and make something great!</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident laudantium aliquam ullam sequi ipsa aperiam mollitia accusantium consequatur temporibus non ad aspernatur alias incidunt possimus maiores veritatis neque harum atque.</p>
+				<h2>Turn searchers into visitors and visitors into customers.</h2>
+				<p>We provide search engine optimisation to small business' in Attleborough and the surrounding areas.</p>
 			</div>
 		</div>
 	</header>
 
 	<div class="container">
 		<div class="row">
-			<div class="span8 offset1" id="main" role="main">
+			<div class="span9" id="main" role="main">
 				<article>
-					<h1>Your content here.</h1>
+					<img src="img/ui/search.png" alt="Search engine optimisation Attleborough icon" class="img-circle border-dark border-fat pull-right featurette-image">
+					<h1>Is your website hard to find?</h1>
+					<p>Search engine optimisation, or simply SEO, is the art of getting your pages ranked high in the search engine results. 85% of internet users use Google for their searching so it's vital that your website is on the first page. In fact, you probably found this very article via a Google search. Trippnology can optimise your existing website and suggest new content to bring in those extra customers.</p>
+					<p>While Google certainly has the "lion's share" of the search market, old favorites like Yahoo and Microsoft's Bing are also very important so we make sure your content will be well indexed by all.</p>
+					<p>We provide you with our custom Google Analytics dashboards which show you the metrics that matter, helping you to further tune your pages and proving that hiring us is good for your business.</p>
+					<p>All SEO work is charged at our flat web services rate available on our <a href="prices">web services pricing</a> page.</p>
+					<p><a href="contact">Contact us</a> about search engine optimisation today and boost your online presence.</p>
+				</article>
+
+				<hr class="divider">
+
+				<article>
+					<h1>Case Study</h1>
+					<img src="img/screens/limecrete.jpg" alt="Screenshot of the Limecrete Company website" class="pull-right roundsm img-polaroid">
+					<p>Trippnology were approached by ecological construction firm <a href="http://www.limecrete.co.uk/" rel="external">The Limecrete Company</a> who provide <a href="http://www.limecrete.co.uk/limecrete.asp" rel="external">limecrete</a>, <a href="http://www.limecrete.co.uk/hempcrete.asp" rel="external">hempcrete</a> and <a href="http://www.limecrete.co.uk/polished-screed.asp" rel="external">polished screed</a> throughout Europe.</p>
+					<p>Louisa and Myles already had a website they had built themselves	using a popular DIY site builder. As with most of these systems, it got	them online but the pages produced were far from search engine friendly	which was hurting their quest for new customers.</p>
+					<p>We completely overhauled the site, simplifying the code and optimising all of the content to improve loading speed, boost their search ranking and pull in new business.</p>
+					<p>Since the redesign, week after week, visitor numbers have continued to grow. Louisa tells us that the number of enquiries tripled almost immediately, leading to them having to take on extra staff to cope with demand for their services.</p>
+					<p>Do you want more customers landing on your website? <a href="contact">Contact us</a> today.</p>
 				</article>
 			</div><!-- /#main -->
 			<div class="span3">
