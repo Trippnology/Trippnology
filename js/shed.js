86495e692426c945d7004a03939b172f40d0e5e9
Trippnology <mail@trippnology.com>
2013-04-22 15:24:36 +0100

    Changed project list to an array.

--- js/shed.js
+++ js/shed.js
@@ -1,5 +1,5 @@
-var projectData = {
-	"0": {
+var projectData = [
+	{
 		"name": "BootstrASP",
 		"description": "A simple framework built with Bootstrap and ASP.",
 		"links": {
@@ -8,7 +8,7 @@ var projectData = {
 			"demo": "http://bootstrasp.trippnology.com"
 		}
 	},
-	"1": {
+	{
 		"name": "BootstraPHP",
 		"description": "Bootstrap based modular framework.",
 		"links": {
@@ -17,7 +17,7 @@ var projectData = {
 			"demo": "http://bootstraphp.trippnology.net"
 		}
 	},
-	"2": {
+	{
 		"name": "Matchbox",
 		"description": "Minimal page template useful for demos.",
 		"links": {
@@ -25,14 +25,14 @@ var projectData = {
 			"github": "https://github.io/Trippnology/matchbox"
 		}
 	},
-	"3": {
+	{
 		"name": "Pastebin",
 		"description": "Text/code sharing, image hosting and URL redirection (shortening coming soon).",
 		"links": {
 			"project": "http://paste.trippnology.net/"
 		}
 	},
-	"4": {
+	{
 		"name": "Trippy's Bittorrent Tracker",
 		"description": "GPL licensed torrent tracker.",
 		"links": {
@@ -40,42 +40,42 @@ var projectData = {
 			"demo": "http://tracker.trippnology.net/"
 		}
 	},
-	"5": {
+	{
 		"name": "IP and Network Tools",
 		"description": "Find out your current real world IP address and more network info.",
 		"links": {
 			"project": "http://trippnology.com/shed/ip"
 		}
 	},
-	"6": {
+	{
 		"name": "Make link",
 		"description": "Sometimes you have a URL but need to make a link to help you save it. This a is a quick and dirty way to do that.",
 		"links": {
 			"project": "http://trippnology.com/shed/makelink"
 		}
 	},
-	"7": {
+	{
 		"name": "Start",
 		"description": "Multi-site search.",
 		"links": {
 			"project": "http://start.trippnology.com/"
 		}
 	},
-	"8": {
+	{
 		"name": "Weather",
 		"description": "Minimal page showing info from a local weather station. Originally built for mobile use.",
 		"links": {
 			"project": "http://trippnology.com/shed/weather"
 		}
 	},
-	"9": {
+	{
 		"name": "YouTube Mature Content Viewer",
 		"description": "This tool makes it easy to watch restricted YouTube content without logging in to YouTube.",
 		"links": {
 			"project": "http://trippnology.com/shed/youtube"
 		}
 	}
-}
+]
 
 var frag = document.createDocumentFragment();
 
