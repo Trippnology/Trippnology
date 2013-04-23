61a1ec55bc2aae6889f71f01db8bb9443ec9ee64
Trippnology <mail@trippnology.com>
2013-04-23 21:59:03 +0100

    Removed social widgets.

--- inc/share-facebook-like.php
+++
@@ -1,7 +0,0 @@
-<div id="fb-root"></div>
-<div class="fb-like"
-data-href="<?=$socialFacebookURL?>"
-data-send="false"
-data-layout="button_count"
-data-width="90"
-data-show-faces="false"></div>
--- inc/share.php
+++
@@ -1,7 +0,0 @@
-<div class="share">
-	<?php
-		include "share-tweetbutton.php";
-		include "share-facebook-like.php";
-		include "share-addthis.php";
-	?>
-</div>
