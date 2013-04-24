240e7cc7de199da4516e27f076a5890e7eb2e620
Trippnology <mail@trippnology.com>
2013-04-25 00:25:15 +0100

    Edited and reordered the bullets.

--- about.php
+++ about.php
@@ -39,33 +39,32 @@ include "head.php";
 						<div class="span4">
 							<ol>
 								<li><span class="lead">Great service is everything</span><br>
-								We’re famous for fast and friendly customer service. We work hard to make sure we live up to that reputation every day. <a href="testimonials">See how our customers rate our service</a>.</li>
-								<li><span class="lead">Clarity is king</span><br>
-								Buzzwords, lingo, and sensationalized sales-and-marketing-speak have no place at Trippnology. We communicate clearly and honestly.</li>
-								<li><span class="lead">Our customers are our investors</span><br>
-								Our customers fund our daily operations by paying for our services. We answer to them — not investors, the stock market, or a board of directors.</li>
-								<li><span class="lead">Green through and through</span><br>
-								We run a paperless office and use a combination of public transport and human power for all our travel, meaning our carbon footprint is tiny.</li>
+								We’re known for fast and friendly customer service. We work hard to make sure we live up to that reputation every day. <a href="testimonials">See how our customers rate our service</a>.</li>
+								<li><span class="lead">Clarity is key</span><br>
+								Buzzwords, lingo, and sensationalized sales and marketing speak have no place at Trippnology. We communicate clearly and honestly.</li>
+								<li><span class="lead">The basics are beautiful</span><br>
+								We’ll never overlook what really matters: The basics. Great service, honest pricing, and respect for our customer’s time, money, and trust.</li>
+								<li><span class="lead">Websites needn't be complicated</span><br>
+								We understand that not everybody is as geeky as we are. That's why the sites we build are easy to use and show off your products and services by giving your customers the best possible experience.</li>
 							</ol>
 						</div>
 						<div class="span4">
 							<ol start="5">
-								<li><span class="lead">The basics are beautiful</span><br>
-								We’ll never overlook what really matters: The basics. Great service, ease of use, honest pricing, and respect for our customer’s time, money, and trust.</li>
 								<li><span class="lead">No hidden fees or secret prices</span><br>
-								We believe everyone is entitled to the best price we can offer. Our prices are public, <a href="prices">published right on our site</a>, and the same no matter who you are.</li>
-								<li><span class="lead">Software should be easy</span><br>
-								Our products are intuitive. You’ll pick them up in seconds or minutes, not hours, days or weeks. We don’t sell you training because you don’t need it.</li>
-								<li><span class="lead">Long-term contracts are obscene</span><br>
-								No one likes being locked into something they don’t want anymore. Our customers can cancel at any time, no questions asked. No setup/termination fees either.</li>
+								We believe everyone is entitled to the best price we can offer. Our prices are public, <a href="prices">published right here on our site</a>, and are the same no matter who you are.</li>
+								<li><span class="lead">Green through and through</span><br>
+								We run a paperless office and use a combination of public transport and human power for all our travel, meaning our carbon footprint is tiny.</li>
+								<li><span class="lead">Our customers are our investors</span><br>
+								Our customers fund our daily operations by paying for our services. We answer to them — not investors, the stock market, or a board of directors.</li>
+								<li><span class="lead">We &hearts; the web</span><br>
+								To us, the world wide web isn't just a marketing tool, it's our passion. When we're not crafting websites for customers, we're experimenting with new technologies, <a href="shed">making tools</a>, and learning every day.</li>
 							</ol>
 						</div>
 					</div>
+				<p class="center"><a href="contact" class="btn btn-large">We would love to hear from you</a></p>
 				</article>
 
-				<hr class="divider roundlrg large">
 
-				<p class="center"><a href="contact" class="btn btn-large">We would love to hear from you</a></p>
 			</div><!-- /.span9 -->
 			<div class="span3">
 				<?php include "inc/nav-block.php"; ?>
