e07abb8cd175c6aa887496cc8ce9129fd7c983da
Trippnology <mail@trippnology.com>
2013-04-26 19:16:36 +0100

    Removed Fancybox.

--- js/script.js
+++ js/script.js
@@ -13,22 +13,7 @@ $(document).ready(function() {
 	$('a.tooltip-left').tooltip({
 		'placement' : 'left'
 	});
-	// Fancybox
-	$("#fancybox-trigger").fancybox({
-		'titlePosition'		: 'inside',
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic'
-	});
 
-// UI.toTop
-/* Change settings here or pass through in the call
-	var defaults = {
-		containerID: 'toTop', // fading element id
-		containerHoverID: 'toTopHover', // fading element hover id
-		scrollSpeed: 1200,
-		easingType: 'linear'
-	};
-*/
 	$().UItoTop({ easingType: 'easeOutQuart' });
 
 	// Make external links open in a new window
@@ -60,6 +45,6 @@ $(document).ready(function() {
 	$('a.scrollme').click(function(){
 		var anchor = $(this).attr('href');
 		scroller(anchor);
-		preventDefault();
+		return false;
 	})
 });
