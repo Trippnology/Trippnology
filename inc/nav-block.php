41ef33a7a2d2a3983d16681ad3c05a32d859f7ce
Trippnology <mail@trippnology.com>
2013-04-26 21:47:28 +0100

    Replaced mistaken deletions.

--- inc/nav-block.php
+++ inc/nav-block.php
@@ -1,5 +1,5 @@
 <div id="nav-block" class="columnleft shadowright roundlrg border-light clearfix" data-spy="affix" data-offset-top="470">
-	<span itemscope itemtype="http://schema.org/Brand"><img src="img/ui/trippnology-logo-square-180.png" alt="Trippnology logo" class="roundlrg aligncenter" itemprop="logo"></span>
+	<span itemscope itemtype="http://schema.org/Brand"><img src="img/ui/trippnology-logo-square-180.png" alt="Trippnology logo" class="roundlrg aligncenter img-polaroid" itemprop="logo"></span>
 	<?php include "inc/menu-vert.php"; ?>
 	<hr class="divider roundsm">
 	<ul class="inline social-links">
