d08b48e09561559ed9a506f984ef7087c1d6ae94
Trippnology <mail@trippnology.com>
2013-04-26 13:02:24 +0100

    Shuffled footer content and added some titles.

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <footer role="contentinfo">
 	<hr class="divider roundsm">
 	<div class="row">
-		<div class="span6">
+		<div class="span4">
 			<p class="lead">Most popular:</p>
 			<ul class="unstyled">
 				<li><a href="web-design-attleborough">Web design Attleborough</a></li>
@@ -9,9 +9,13 @@
 				<li><a href="it-support-attleborough">IT support Attleborough</a></li>
 			</ul>
 		</div>
-		<div class="span6">
+		<div class="span4">
+			<p class="lead">Social</p>
+			<p>Follow us on <a href="http://twitter.com/<?=$socialTwitterName?>" rel="external">twitter</a>, <a href="https://github.com/Trippnology" rel="external">github</a>, <a href="http://bitbucket.org/Trippnology" rel="external">Bitbucket</a> or <a href="<?=$socialFacebookURL?>" rel="external">Facebook</a>.</p>
+		</div>
+		<div class="span4">
+			<p class="lead">Meta</p>
 			<?php include "inc/review-score.php"; ?>
-			<p>Follow us on <a href="<?=$socialFacebookURL?>" rel="external" title="<?=$siteName?> Facebook Page">Facebook</a> and <a href="http://twitter.com/<?=$socialTwitterName?>" rel="external" title="<?=$siteName?> on twitter">twitter</a>.</p>
 			<p><a href="http://creativecommons.org/licenses/by-sa/2.0/uk/" rel="license"><img src="img/ui/cc-by-sa-80x15.png" alt="Creative Commons Attribution-ShareAlike 2.0 UK: England &amp; Wales license"></a> <a href="<?=$siteDomain?>" title="<?=$siteName?>"><?=$siteName?></a></p>
 		</div>
 	</div>
