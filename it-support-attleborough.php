7d78b117b74453a42ba79705e7abe8484259e1de
Trippnology <mail@trippnology.com>
2013-04-26 15:26:17 +0100

    Added good times text.

--- it-support-attleborough.php
+++ it-support-attleborough.php
@@ -34,7 +34,7 @@ include "head.php";
 					<h1>Things go wrong and technology can play up sometimes.</h1>
 					<div class="well white span2 pull-right">
 						<p><span class="lead">Bottom line:</span> IT downtime costs your business. If you need emergency IT support in Attleborough, we could be there within an hour!</p>
-						<p><a class="btn btn-mini btn-success" href="contact"><span data-title="Let's talk »">SEND HELP!</span></a></p>
+						<p><a class="btn btn-mini btn-success" href="contact">SEND HELP!</a></p>
 					</div>
 					<p>If something goes wrong with your IT, it can leave your staff unable to continue with their work. This where we come in, with our passion for problem solving. Our customers soon realise we are the people to call whenever issues crop up.</p>
 					<p>We've built up a wealth of real world experience over many years of diagnosing problems with hardware, software (both off the shelf and bespoke), operating systems, networks, broadband and user behaviour. This experience allows us to quickly pinpoint the issue, minimising costly downtime.</p>
@@ -46,7 +46,14 @@ include "head.php";
 
 				<article>
 					<h1>Not just for the bad times!</h1>
+					<div class="well white span2 pull-right">
+						<p><span class="lead">Bottom line:</span> If you're looking to add some new equipment, improve your broadband or just need some advice, we're here to help.</p>
+						<p><a class="btn btn-mini btn-success" href="contact">Let's talk »</a></p>
+					</div>
+					<p>We have our finger on the pulse of the IT world, keeping up to date with the latest hardware and software so you don't have to.</p>
+					<p>If your business relies on the internet for it's daily operations, you need your connection to be reliable and for any faults that do occur, to be quickly rectified. Trippnology can order, install and setup business grade broadband, ensuring your business stays connected.</p>
 					<p>Perhaps you're looking to expand and would like some purchasing advice? Or have some equipment that you're not using to it's full potential? We can help with that too, showing you the right tools for the job and making the most of your existing IT investment.</p>
+					<h2 class="center">I need expert IT advice... <a class="btn btn-large btn-success" href="contact">Let's talk »</a></h2>
 				</article>
 			</div><!-- /#main -->
 			<div class="span3">
