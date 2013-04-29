f4890954ea240b5f8017cf7fa00b39fcb6616e5f
Trippnology <mail@trippnology.com>
2013-04-29 12:29:48 +0100

    Switched to minified bootstrap CSS.

--- head.php
+++ head.php
@@ -9,7 +9,7 @@
 	<meta name="robots" content="index,follow">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css"><!-- Placed last to override all other styles -->
 	<link rel="canonical" href="<?=$siteDomain?><?=$strCanonical?>">
 	<link rel="author" href="<?=$siteDomain?>humans.txt">
