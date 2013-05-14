1c0139f51c36224af74782c5669b871e8f25fa9c
Trippnology <mail@trippnology.com>
2013-05-14 14:49:01 +0100

    Added Terms and conditions page. Fixes #21

---
+++ terms-and-conditions.php
@@ -0,0 +1,75 @@
+<?php
+include "inc/settings.php";
+
+$strPageName = "Terms and Conditions";
+$strCanonical = "template-blank.php";
+$strDescription = "Our standard terms and conditions.";
+$strKeywords = "terms, conditions";
+$SEOtitle = $strPageName . " | " . $siteName;
+$pageSlug = "terms";
+$pageHeaderImageURL = "img/ui/headers/trippnology-default.jpg";
+$pageHeaderImageURLSource = "https://secure.flickr.com/photos/manchester-monkey/4150657215/";
+$strTweetText = $strPageName;
+
+include "head.php";
+?>
+
+<body class="page-<?=$pageSlug?>">
+	<?php include "inc/navbar.php"; ?>
+
+	<header class="page-header roundlrg">
+		<img src="<?=$pageHeaderImageURL?>" alt="" class="roundlrg" data-sourceurl="<?=$pageHeaderImageURLSource?>">
+		<div class="container">
+			<div class="header-caption roundlrg">
+				<h1 id="pagetitle"><a href="<?= $strCanonical ?>" title="<?= $strPageName ?>"><?= $strPageName ?></a></h1>
+				<h2>What you can expect from us and what we expect from you.</h2>
+			</div>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="row">
+			<div class="span9" id="main" role="main">
+				<article>
+					<h1>Our standard terms and conditions</h1>
+					<p>Revised: 14/05/2013</p>
+					<p class="lead">Please ensure that you read and understand all of these conditions.</p>
+					<h2>General</h2>
+					<p><span class="lead">Contract:</span> The client's approval for work to commence shall be deemed a contractual agreement between the client and Trippnology. Important: Approval for the work to commence and/or payment of the advance fee indicates that the client accepts the terms and conditions outlined in this document.</p>
+					<p><span class="lead">Travel Time and Expenses:</span> Traveling time to and from customer premises is not generally included in an estimate. Trippnology reserve the right to make a charge for traveling time at our normal consultancy rates. Likewise Trippnology reserve the right to charge for travelling expenses based on 45p per mile. (NOTE: There will be no charges for travelling time or expenses incurred before you give your approval for work to commence.)</p>
+					<p><span class="lead">Payment terms:</span> All invoices are due within 7 days of invoice date. Our preferred payment method is via bank transfer. If you manage to talk us into accepting a cheque and your cheque is subsequently returned by the bank as unpaid for any reason, you will be liable for a returned cheque charge of &pound;25.</p>
+					<p><span class="lead">Late Payment:</span> Accounts that have not been settled within 30 days of original invoice date will incur a late payment charge of 1.5% of the amount outstanding. You will also be charged statutory interest of 8% above the reference rate (fixed for the six month period within which date the invoices became overdue) pursuant to the late payment legislation.</p>
+
+					<h3>Web design/development</h3>
+					<p><span class="lead">Advance Payment:</span> An advance of 50% of the total cost of the project is required before work can commence. After work commences, this is non-refundable.</p>
+					<p><span class="lead">Payment:</span> Full publication of the web pages we have designed may take place only after full payment has been received. Any material previously published may be removed if payment is not received. When this occurs, a minimum charge of &pound;50 will be required to have the site restored.</p>
+					<p><span class="lead">Registration Charges:</span> All costs arising from the registration of a domain name shall be met by the Client. Trippnology registers domain names in the client's name so that they retain full ownership of these. As long as your account is in good standing, Trippnology agree to transfer domain names to the client immediately, upon request and without charge.</p>
+					<p><span class="lead">Copyright:</span> Trippnology will hold intellectual copyright of any material, including any source code and original images created for the client until payment of the final invoice. At this time Trippnology will license this intellectual copyright to the client for as long as we are retained to look after your website.</p>
+					<p><span class="lead">Clients Responsibilities with Regard to Copyright:</span> In situations where the client provides images, text, animations or any other content for their website, they are legally responsible for ensuring that this material does not infringe any copyright. Certain images provided by Trippnology in the construction of the website may have been purchased under license from stock image suppliers. These images are generally only licensed for use on a single website and may not be used in publicity material. The website owner is legally responsible for ensuring that this does not happen. If you wish to use any images from the site for other purposes please <a href="contact">contact us</a> for clarification.</p>
+					<p><span class="lead">Search Engine Promotion:</span> Trippnology is not responsible for the client's on-going web site promotion. Should the client require the site to be promoted on an ongoing basis, a separate contract must be agreed. The order in which websites are ranked in the natural search results is controlled by the search engines. While Trippnology can optimise your site for this, Trippnology are unable to make any guarantees about the success of any search engine promotion activity.</p>
+					<p><span class="lead">Failure to Provide Required Website Content:</span> To remain efficient, Trippnology must ensure that work we have programmed is carried out at the scheduled time. On occasions, Trippnology may have to reject other work and enquiries to ensure that your work is completed at the time arranged. This is why Trippnology asks that you provide all the required information in advance. On any occasion where Trippnology cannot progress your website because you have not provided the required information when you have agreed to do so, and Trippnology are delayed as result, Trippnology reserve the right to impose a surcharge of up to 25%. Also, if your job involves Search Engine Optimisation Trippnology need the text content for your site in advance so that the SEO can be planned and completed efficiently. If you agree to provide Trippnology with the required information and subsequently fail to do within four weeks of project commencement, Trippnology reserve the right to close the project and the balance remaining becomes payable immediately. Simply put, all the above condition says is do not give Trippnology the go ahead to start until you are ready to do so. NOTE: Text content should be delivered as a text file (or similar) with the pages or sections in the supplied document clearly marked to represent the content of the relevant pages on your website.</p>
+					<p><span class="lead">Conceptualising:</span> Conceptualising is the process of producing website concepts for clients. Concepts can include site mock ups, graphics and design proposals. For higher cost websites, this will be included but for low cost sites (below %pound;1000) please bear in mind that unless previously agreed, only one concept is possible. You should therefore ensure that you let Trippnology have your preferred colour scheme and design requirements beforehand. If you don't do this, Trippnology will design the website appropriately but if it is not to your taste, Trippnology may not be able to rework the website without additional charges. This is why it is often best to show Trippnology other websites that you like as an example of what you require.</p>
+					<p><span class="lead">Quotations:</span> The price quoted to the client is for the work agreed on the quotation only. Should the client decide that changes are required after work on the website commences, then Trippnology will accept these changes with the provision that additional charges may have to be negotiated.</p>
+					<p><span class="lead">Cancellation:</span> Should the client wish to cancel at any point during the build process, they shall remain liable for the work that has taken place and shall be invoiced accordingly.</p>
+					<p><span class="lead">Future Support:</span> The website is provided to and accepted by the client as a fully functioning, completed work. Excluding web hosting issues, Trippnology is not responsible for future support. Support can be provided upon request and for an agreed fee. No guarantee of future support is given unless an ongoing support package is negotiated.</p>
+					<p><span class="lead">Future Site Problems:</span> Unfortunately malicious software, spyware, viruses and website hacking are facts of life on today's Internet. It is highly unlikely that these will affect your website, and Trippnology will endeavour to protect it from this during its creation. Trippnology cannot be held responsible for problems that develop on completed sites as a result of illegal activity.</p>
+					<p><span class="lead">Compliance with Ecommerce, Accessibility or Other Regulations:</span> Trippnology design websites in accordance with the client's specifications. It is the client's responsibility to ensure that the website and its content comply with standing regulations. Trippnology cannot accept responsibility for any failure to comply with regulations related to accessibility, selling online or those related to a specific business or trade. Trippnology can research this on the client's behalf upon request, but in any business where complex compliance issues exist, Trippnology recommend that the client takes legal advice from their company lawyer.</p>
+					<p class="lead">If you require clarification about any of these terms and conditions, please <a href="contact">contact us</a> and we will be happy to discuss them with you.</p>
+				</article>
+			</div><!-- /#main -->
+			<div class="span3">
+				<?php include "inc/nav-block.php"; ?>
+			</div>
+		</div><!-- /.row -->
+
+		<div class="row">
+			<div class="span12">
+				<?php include "inc/footer.php"; ?>
+			</div>
+		</div>
+    </div>
+
+	<?php include "scriptblock.php"; ?>
+
+</body>
+</html>
