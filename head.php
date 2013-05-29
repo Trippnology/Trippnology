8cb86b679a41dcf28b6d03b7d2ac4a70ad675758
Trippnology <mail@trippnology.com>
2013-05-29 14:18:53 +0100

    Added geo meta tags. Fixes #34

--- head.php
+++ head.php
@@ -8,6 +8,10 @@
 	<meta name="keywords" content="<?=$strKeywords?>">
 	<meta name="robots" content="index,follow">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta content="GB-NFK" name="geo.region">
+	<meta content="Attleborough" name="geo.placename">
+	<meta content="52.519450;1.047002" name="geo.position">
+	<meta content="52.519450, 1.047002" name="ICBM">
 
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css"><!-- Placed last to override all other styles -->
