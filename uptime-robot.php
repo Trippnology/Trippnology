cd588d45b02a3bc1cc0f387fbd3113c1520f4bc9
Trippnology <trippnology@gmail.com>
2013-09-15 17:15:26 +0100

    Added uptime-robot.php

---
+++ uptime-robot.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Uptime Robot</title>
+</head>
+
+<body>
+	<p>Thanks <a href="http://www.uptimerobot.com/">Uptime Robot</a>.</p>
+</body>
+</html>
