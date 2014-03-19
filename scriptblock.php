5cda2c839e0ba6e360de4312b8de6b80931f8b22
Trippnology <trippnology@gmail.com>
2014-03-19 12:53:37 +0000

    Hosting updated jQuery locally.

--- scriptblock.php
+++ scriptblock.php
@@ -1,7 +1,4 @@
-<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write("<script src='js/libs/jquery-1.7.1.min.js'>\x3C/script>")</script>
-
+<script src="js/libs/jquery-1.11.0.min.js"></script>
 <script src="js/libs/jquery-easing.1.3.0.min.js"></script><!-- Used by back-to-top plugin -->
 <script src="js/libs/bootstrap.min.js"></script>
 <!-- //-beg- concat_js -->
