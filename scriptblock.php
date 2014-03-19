8b4eae5414e08a5bddad1e53fc8845132a689878
Trippnology <trippnology@gmail.com>
2014-03-19 12:48:56 +0000

    Hosting jQuery easing plugin locally.

--- scriptblock.php
+++ scriptblock.php
@@ -2,10 +2,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script>window.jQuery || document.write("<script src='js/libs/jquery-1.7.1.min.js'>\x3C/script>")</script>
 
-<!-- Grab extra jQuery files from CDN or fall back to local -->
-<script src="//cachedcommons.org/cache/jquery-easing/1.3.0/javascripts/jquery-easing-min.js"></script>
-<script>window.jQuery || document.write("<script src='js/libs/jquery.easing-1.3.pack.js'>\x3C/script>")</script>
-
+<script src="js/libs/jquery-easing.1.3.0.min.js"></script><!-- Used by back-to-top plugin -->
 <script src="js/libs/bootstrap.min.js"></script>
 <!-- //-beg- concat_js -->
 <script src="js/plugins.js"></script>
