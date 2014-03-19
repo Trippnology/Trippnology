740c0cb46af544dbfd35b957c4132fa1d4dad6bb
Trippnology <trippnology@gmail.com>
2014-03-19 12:58:40 +0000

    Moved jQuery Easing into plugins file. Derp.

--- scriptblock.php
+++ scriptblock.php
@@ -1,5 +1,4 @@
 <script src="js/libs/jquery-1.11.0.min.js"></script>
-<script src="js/libs/jquery-easing.1.3.0.min.js"></script><!-- Used by back-to-top plugin -->
 <script src="js/libs/bootstrap.min.js"></script>
 <!-- //-beg- concat_js -->
 <script src="js/plugins.js"></script>
